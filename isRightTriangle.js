7c9aaaf0560d8d973254d6b816761494a3bf6e6a
Mahin35 <mahin.shahrier@stuysu.org>
2020-11-29 22:34:48 -0500

    Fixed code again, finally

--- isRightTriangle.js
+++ isRightTriangle.js
@@ -2,6 +2,7 @@ var _ = require('lodash');
 
 const isRightTriangle = (a, b, c) => {
     if (a === 0 || b === 0 || c === 0) return false;
+    if (a < 0 || b < 0 || c < 0) return false;
     let args = [a, b, c];
     let randomNumber = _.sample(args);
     for (let i = 0; i < 10000; i++) {
@@ -22,4 +23,5 @@ console.log(isRightTriangle(3,4,6)) //False
 console.log(isRightTriangle(3,4,5)) //True
 console.log(isRightTriangle(4,5,3)) //True
 console.log(isRightTriangle(0,0,0)) //False
+
 
