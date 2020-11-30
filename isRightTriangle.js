3632d09aedda3f6a329797b60e11683f5eb7d392
Mahin35 <mahin.shahrier@stuysu.org>
2020-11-29 22:07:37 -0500

    Fixed Code

--- isRightTriangle.js
+++ isRightTriangle.js
@@ -1,10 +1,25 @@
+var _ = require('lodash');
+
 const isRightTriangle = (a, b, c) => {
     if (a === 0 || b === 0 || c === 0) return false;
-    return (a**2 +b**2 === c**2);
+    let args = [a, b, c];
+    let randomNumber = _.sample(args);
+    for (let i = 0; i < 10000; i++) {
+        let random1 = _.sample(args);
+        let random2 = _.sample(args);
+        let random3 = _.sample(args);
+
+        if (random1 ** 2 + random2 ** 2 === random3 ** 2) {
+            return true;
+        }
+    }
+    return false;
 }
 
 module.exports = isRightTriangle;
 
 console.log(isRightTriangle(3,4,6)) //False
 console.log(isRightTriangle(3,4,5)) //True
+console.log(isRightTriangle(4,5,3)) //True
 console.log(isRightTriangle(0,0,0)) //False
+
