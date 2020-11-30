583f6bba0a0a1c433eaf6d676f31b1071873e99c
Mahin35 <mahin.shahrier@stuysu.org>
2020-11-29 20:42:12 -0500

    Optomized Function for (0,0,0)

--- isRightTriangle.js
+++ isRightTriangle.js
@@ -1,6 +1,10 @@
-const isRightTriangle = (a, b, c) => (a**2 +b**2 === c**2)
+const isRightTriangle = (a, b, c) => {
+    if (a === 0 || b === 0 || c === 0) return false;
+    return (a**2 +b**2 === c**2);
+}
 
 module.exports = isRightTriangle;
 
-console.log(isRightTriangle(3,4,6)) // False
+console.log(isRightTriangle(3,4,6)) //False
 console.log(isRightTriangle(3,4,5)) //True
+console.log(isRightTriangle(0,0,0)) //False
